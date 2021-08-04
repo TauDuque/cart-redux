a7f7d5ccbe7abd657c4d2b5b6d162e40c6d1d232
TauDuque <hectauduque@gmail.com>
2021-08-04 13:25:40 -0300

    new project

---
+++ src/reducer.js
@@ -0,0 +1,13 @@
+import {
+  INCREASE,
+  DECREASE,
+  CLEAR_CART,
+  GET_TOTAL,
+  GET_AMOUNT,
+} from "./actions";
+
+function reducer(state, action) {
+  return state;
+}
+
+export default reducer;
